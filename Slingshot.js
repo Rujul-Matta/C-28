34081954fd0fef8021235174a59d49deda793f8b
Rujul-Matta <latamatta1205@gmail.com>
2020-11-18 16:10:58 +0530

    First change

---
+++ Slingshot.js
@@ -0,0 +1,23 @@
+class Sling{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB;
+        World.add(wolrd, this.pointB);
+        
+        this.Sling = Constraint.create(options);
+        World.add(world, this.Sling);
+    }
+
+    display(){
+        var pointA = this.Sling.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+    
+}
